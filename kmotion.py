73eef8abf7e9c8eca4707c6a5b55067ab8fb1699
Thomas Kraus <kraust@vmware.com>
2019-06-04 20:41:02 -0700

    Testing cant see backup on recovery cluster. Removed velero backup describe.

--- kmotion.py
+++ kmotion.py
@@ -19,7 +19,7 @@ def main():
 
     active_index = contexts.index(active_context['name'])
     # PICK to select SOURCE Cluster for KMOTION
-    cluster1, first_index = pick(contexts, title="Pick the source Cluster Context for the POD to be backed up",
+    cluster1, first_index = pick(contexts, title="Pick the source Cluster Context for the POD to kmotioned",
                                  default_index=active_index)
     client1 = client.CoreV1Api(
         api_client=config.new_client_from_config(context=cluster1))
@@ -31,7 +31,7 @@ def main():
     selected_pod = pick(source_pods, title="Pick the POD to be backed up")
 
     # PICK to select Destination Cluster for KMOTION
-    cluster2, _ = pick(contexts, title="Pick the target Cluster Context for the POD to be restored to",
+    cluster2, _ = pick(contexts, title="Pick the target Cluster Context for the POD to be kmotioned to",
                        default_index=first_index)
     client2 = client.CoreV1Api(
         api_client=config.new_client_from_config(context=cluster2))
@@ -39,7 +39,7 @@ def main():
 
     for i in client1.list_pod_for_all_namespaces().items:
         if selected_pod[0] == i.metadata.name: # Return the Kubernetes API POD object
-            print("Found the POD Object for Selected POD")
+            print("--Found the POD Object for Selected POD")
             source_pod_object = i
 
     print ('This is the POD you selected {0}'.format(source_pod_object.metadata.name))
@@ -70,54 +70,13 @@ def main():
     backup_create_cmd = ['velero', 'backup', 'create', backup_name, '--selector', selector, '-w', '--kubecontext', cluster1]
     subprocess.check_call(backup_create_cmd)
 
-    # CAN PROBABLY GET RID OF THIS EVENTUALLY
-    # VELERO BACKUP Status
-    #DEBUG backup_query_cmd = ['velero', 'backup', 'describe', backup_name, '--kubecontext',cluster1]
-    #DEBUG subprocess.check_call(backup_query_cmd)
-
-    time.sleep(3)
-
-    '''# DEBUG for TESTING - VELERO BACKUP Delete
-    backup_delete_cmd = ['velero', 'backup', 'delete', backup_name, '--kubecontext', cluster2, '--confirm']
-    subprocess.check_call(backup_delete_cmd)
-    print("DEBUG - Sleeping for 25 seconds")
-    time.sleep(25)
-    
-
-    #DEBUG Check the existence of backup on the DESTINATION side.
-    backup_get_cmd = ['velero', 'backup', 'get', '--kubecontext', cluster2]
-    subprocess.check_call(backup_get_cmd)
-    '''
-
-
-
-    '''
-    while (output.indexOf(backup_name) < 0):
-        print("NEW ouput Waiting for Backup to be synchronized with Recovery Cluster . . . ")
-        time.sleep(4)
-    else:
-        print("NEW output Velero backup exists on Recovery Cluster. Moving to next step.")
-        # return
-    '''
-    '''
-    while (output.find(backup_name) == -1):
-        print("Waiting for backup ", backup_name, " to be synchronized with Recovery Cluster ", cluster2)
-        print("output velero get ", output)
-        print("backup_name.encode", backup_name)
-        print("Find Integer value", output.find(backup_name))
-        time.sleep(4)
-
-    print("Velero backup ", backup_name ," exists on Recovery Cluster ", cluster2, ". Moving to next step.")
-    '''
     # Work to interpret results of velero backup get
     while True:
         output = subprocess.check_output(['velero', 'backup', 'get', '--kubecontext', cluster2]).decode()
-        print(type(output))
-        print(output)
         print("Waiting for backup ", backup_name, " to be synchronized with Recovery Cluster ", cluster2)
-        print("output velero get ", output)
-        print("backup_name.encode", backup_name)
-        print("Find Integer value", output.find(backup_name))
+        # DEBUG print("output velero get ", output)
+        # DEBUG print("backup_name.encode", backup_name)
+        # DEBUG print("Find Integer value", output.find(backup_name))
         time.sleep(3)
         if (output.find(backup_name) != -1):
             print("Velero backup ", backup_name, " exists on Recovery Cluster ", cluster2, ". Moving to next step.")
@@ -137,12 +96,11 @@ def main():
 
     print('KMotioning POD {0} from {1} cluster to {2} cluster... '.format(source_pod_object.metadata.name, cluster1, cluster2))
 
-    '''
+
     print("\n\nList of pods on %s:" % cluster2)
     for i in client2.list_pod_for_all_namespaces().items:
         print("%s\t%s\t%s" %
               (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-    '''
 
 if __name__ == '__main__':
     main()
