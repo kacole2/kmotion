c7c284c49206164669ff7c33b18c22f47b9bed63
Thomas Kraus <kraust@vmware.com>
2019-06-04 19:58:08 -0700

    Testing cant see backup on recovery cluster. Removed velero backup describe.

--- kmotion.py
+++ kmotion.py
@@ -101,17 +101,14 @@ def main():
         print("NEW output Velero backup exists on Recovery Cluster. Moving to next step.")
         # return
     '''
-    while(True):
-        if (output.find(backup_name) == -1):
-            print("Waiting for backup ", backup_name, " to be synchronized with Recovery Cluster ", cluster2)
-            #DEBUG print("output velero get ", output)
-            #DEBUG print("backup_name.encode", backup_name)
-            print("Find Integer value", output.find(backup_name))
-            time.sleep(4)
-            continue
-        elif (output.find(backup_name) >= 0):
-            print("Velero backup ", backup_name ," exists on Recovery Cluster ", cluster2, ". Moving to next step.")
-            break
+    while (output.find(backup_name) == -1):
+        print("Waiting for backup ", backup_name, " to be synchronized with Recovery Cluster ", cluster2)
+        #DEBUG print("output velero get ", output)
+        #DEBUG print("backup_name.encode", backup_name)
+        print("Find Integer value", output.find(backup_name))
+        time.sleep(4)
+
+    print("Velero backup ", backup_name ," exists on Recovery Cluster ", cluster2, ". Moving to next step.")
 
     # VELERO Restore
     restore_create_cmd = ['velero', 'restore', 'create', backup_name, '--from-backup', backup_name, '-w','--kubecontext',cluster2]
